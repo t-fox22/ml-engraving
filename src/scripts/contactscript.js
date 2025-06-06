f1a0ff28c0bb2f8c6bcd031f8188b0ba6e6c1651
Tomas <tfox0020@gmail.com>
2025-06-06 20:33:46 +0100

    Update contact page and add gallery page

---
+++ src/scripts/contactscript.js
@@ -0,0 +1,18 @@
+const form = document.getElementById("whatsappForm");
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = encodeURIComponent(document.getElementById("name")?.value || "");
+  const email = encodeURIComponent(
+    document.getElementById("email")?.value || ""
+  );
+  const messageText = encodeURIComponent(
+    document.getElementById("message")?.value || ""
+  );
+
+  const fullMessage = `Hello, my name is ${name}. My email is ${email}. ${messageText}`;
+  // const phone = '353876336306';
+  const phone = "353851551827";
+  const url = `https://wa.me/${phone}?text=${fullMessage}`;
+
+  window.open(url, "_blank");
+});
